a0cbdfd4fc7560e39698f17bcc6ed0a1a8ec8daf
github username <github email address>
2020-07-15 18:57:07 +0300

    removed example components and implemented home page with navbar

---
+++ rent-smart-frontend/src/features/Home.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const Home = () => {
+
+    const [searchText, setSearchText] = useState('')
+
+    const onChange = e => setSearchText(e.target.value)
+    
+    const handleOnSubmit = event => {
+        event.preventDefault();
+        console.log("Submitted")
+    }
+
+    return (
+        <div>
+            <h3>Renting is an investment.</h3>
+            <h1>Rent Smart.</h1>
+            <form onSubmit={e => handleOnSubmit(e)}>
+                <input value={searchText} onChange={onChange}/>
+                <input type="submit" value="Search" />
+            </form>
+        </div>
+    );
+};
+
+export default Home;
---
+++ rent-smart-frontend/src/features/Navbar.js
@@ -0,0 +1,33 @@
+import React/*, { useState }*/ from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+const NavBar = () => {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="/">Rent Smart</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Write a Review</Nav.Link>
+                    </Nav>
+                    <Nav className="ml-auto">
+                        <Nav.Link href="/signin">Sign In</Nav.Link>
+                        <Nav.Link href="/signup">Sign Up</Nav.Link>
+                        <NavDropdown title="Profile" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/messages">Messages</NavDropdown.Item>
+                            <NavDropdown.Item href="/reviews">Reviews</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="/account">Account</NavDropdown.Item>
+                            <NavDropdown.Item href="/logout">Log out</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    )
+}
+
+export default NavBar;
