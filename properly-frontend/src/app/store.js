d40181d06c33f2c38098277249664f3ca26ef611
dlyons8 <dlyons8@binghamton.edu>
2020-08-11 17:01:30 -0400

    added authReducer

--- properly-frontend/src/app/store.js
+++ properly-frontend/src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/authSlice';
 
 export default configureStore({
   reducer: {
- 
+    authorization: authReducer
   },
 });
