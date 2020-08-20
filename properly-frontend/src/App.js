0166ca297ac36466f41af05988dd485193f31bb1
dlyons8 <dlyons8@binghamton.edu>
2020-08-19 22:46:30 -0400

    added new routes and switch from react-router-dom

--- properly-frontend/src/App.js
+++ properly-frontend/src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom';
 import './App.css';
 import Navbar from './features/Navbar';
 import Home from './features/Home';
 import Signin from './features/Signin';
 import Signup from './features/Signup';
+import LandlordSearchDisplay from './features/LandlordSearchDisplay';
+import MapSearchDisplay from './features/MapSearchDisplay';
 
 function App() {
+
   return (
     <Router>
       <div>
         <Navbar />
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/signin" component={Signin}/>
-        <Route exact path="/signup" component={Signup}/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/signin" component={Signin}/>
+          <Route exact path="/signup" component={Signup}/>
+          <Route path="/landlords" component={LandlordSearchDisplay}/>
+          <Route path="/locations" component={MapSearchDisplay}/>
+        </Switch>
       </div>
     </Router>
   );
