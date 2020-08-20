2862418f73b17a512f48fe1c0270a0658dd9e6bc
dlyons8 <dlyons8@binghamton.edu>
2020-08-19 22:45:15 -0400

    implemented google search autocomplete

--- properly-frontend/src/features/Home.js
+++ properly-frontend/src/features/Home.js
@@ -3,38 +3,125 @@ import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
+import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete';
+import useOnclickOutside from "react-cool-onclickoutside";
+import { Redirect } from 'react-router-dom'
 
 const Home = () => {
 
-    const [searchText, setSearchText] = useState('')
-    const [value, setValue] = useState("landlord");
+    const [option, setOption] = useState("landlord");
+    const [redirectForSearch, setRedirectForSearch] = useState(false);
+
+
+    const {
+        ready,
+        value,
+        suggestions: { status, data },
+        setValue,
+        clearSuggestions,
+      } = usePlacesAutocomplete({
+        requestOptions: {
+          /* Define search scope here */
+          types: ['address']
+        },
+        debounce: 200,
+      });
 
     const handleChange = (val) => { 
-        setValue(val.currentTarget.dataset.rbEventKey);
+        setOption(val.currentTarget.dataset.rbEventKey);
     }
 
     const placeHolder = () => {
-        return value === "landlord" ? "Search by landlord name" : "Search by address"
+        return option === "landlord" ? "Search by landlord name" : "Search by address"
     } 
 
-    const onChange = e => setSearchText(e.target.value)
+    const handleInput = (e) => {
+        // Update the keyword of the input element
+        setValue(e.target.value);
+    };
+
+    const renderRedirect = () => {
+        if (option === "landlord") {
+            return (<Redirect to={{
+                pathname: `/landlords`,
+                search: "?name=" + value,
+                state: { name: value }
+              }}/>)
+        } else {
+           return(<Redirect
+            to={{
+              pathname: "/locations",
+              search: "?address=" + value,
+              state: { address: value }
+            }}
+          />)
+    }}
 
     
     const handleOnSubmit = event => {
         event.preventDefault();
-        console.log("Submitted")
-        setSearchText('')
+        if (value.length >= 3) {
+            setRedirectForSearch(true)
+        }
     }
 
+    const ref = useOnclickOutside(() => {
+    // When user clicks outside of the component, we can dismiss
+    // the searched suggestions by calling this method
+        clearSuggestions();
+    });
+    
+    const handleSelect = ({ description }) => () => {
+        // When user selects a place, we can replace the keyword without request data from API
+        // by setting the second parameter to "false"
+        setValue(description, false);
+        clearSuggestions();
+ 
+        // Get latitude and longitude via utility functions
+        getGeocode({ address: description })
+        .then((results) => getLatLng(results[0]))
+        .then(({ lat, lng }) => {
+            console.log("📍 Coordinates: ", { lat, lng });
+        })
+        .catch((error) => {
+            console.log("😱 Error: ", error);
+        });
+    };
+    
+    const renderSuggestions = () =>
+        data.map((suggestion) => {
+        const {
+            id,
+            structured_formatting: { main_text, secondary_text },
+        } = suggestion;
+        
+        return (
+        <div key={id} onClick={handleSelect(suggestion)}>
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </div>
+        );
+    });
+ 
+
     return (
-        <div>
+        <>
+        {redirectForSearch ? renderRedirect() : null}
+        <div ref={ref}>
+            
             <h3>The only way to rent</h3>
             <h1>Properly</h1>
             <Form onSubmit={e => handleOnSubmit(e)}>
                 <Form.Row>
                     <Col>
-                    <Form.Control placeholder={placeHolder()} value={searchText} onChange={onChange} />
-                    <Nav variant="pills" defaultActiveKey={value}>
+                    <Form.Control placeholder={placeHolder()} value={value} onChange={handleInput} disabled={!ready} />
+                    {/* We can use the "status" to decide whether we should display the dropdown or not */}
+                    {option === "address" && status === "OK" && <ul>{renderSuggestions()} <div className="logo">
+                      <img
+                        src="https://developers.google.com/maps/documentation/images/powered_by_google_on_white.png"
+                        alt="Powered by Google"
+                      />
+                    </div></ul>}
+                    <Nav variant="pills" defaultActiveKey={option}>
                         <Nav.Item>
                             <Nav.Link eventKey="landlord" onClick={handleChange}>Find a Landlord</Nav.Link>
                         </Nav.Item>
@@ -51,6 +138,7 @@ const Home = () => {
                 </Form.Row>
             </Form>
         </div>
+        </>
     );
 };
 
